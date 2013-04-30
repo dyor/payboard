2cd3c58f6c7fe621cd654d95deabe57948529f80
Avilay Parekh <avilay@gmail.com>
2013-04-29 23:09:45 -0700

    updating gitignore to not upload environment file

--- payboard.rb
+++ payboard.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'stripe'
 
 get '/' do
   "Welcome to Payboard!"
+end
+
+get '/test/customers' do
+  Stripe.api_key = 'sk_test_Nc9GpIjxcYSFf03SgMNWsf9z'
+  cust_names = Stripe::Customer.all.map{|c| c.description}.join(',')
 end
