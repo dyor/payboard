b5671e8798124f0fb9b2491cd04aefcb921996e9
Avilay Parekh <avilayparekh@hotmail.com>
2013-04-30 19:39:27 -0700

    charting spike complete

--- payboard.rb
+++ payboard.rb
@@ -11,25 +11,18 @@ configure do
   set :client_id, ENV['STRIPE_CLIENT_ID']
   set :my_api_key, ENV['STRIPE_API_KEY']
   set :state, Digest::SHA2.hexdigest(rand.to_s)
+
+  set :bind, '192.168.106.128'
 end
 
 get '/' do
-  logger.info("Inside root. rack.logger = #{ENV['rack.logger']}. logger.class = #{logger.class}")
-  authorize_url = OAuth2Client.new(settings).authorize_url
+  @authorize_url = OAuth2Client.new(settings).authorize_url
   session[:api_key] = settings.my_api_key
-  html = <<-HTML
-    <html>
-      <body>
-        <h1>Welcome to Payboard!</h1>
-        <a href="#{authorize_url}">
-          Connect to Stripe
-        </a><br/>
-        <a href="/customers">
-          Get customers
-        </a>
-      </body>
-    </html>
-  HTML
+  @categories = ['Jun-12', 'Jul-12', 'Aug-12', 'Sep-12', 'Oct-12', 'Nov-12', 'Dec-12', 'Jan-13', 'Feb-13', 'Mar-13']
+  @silver = [2, 3, 2, 4, 8, 10, 6, 6, 7, 6]
+  @meetup = [1, 3, 1, 1, 3, 6, 6, 12, 26, 28]
+  @bronze = [4, 9, 11, 17, 20, 21, 26, 27, 21, 22]  
+  erb :placeholder
 end
 
 get '/testlog' do
